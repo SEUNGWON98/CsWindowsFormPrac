84895d7f8d5c6805d21db363f6f1fd6022ab0c6b
datanet <datanet@DESKTOP-TP6SK6S>
2024-03-15 16:17:41 +0900

    초기 커밋.

---
+++ Demo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("반복이네");
+
+int age = 14;
+
+Console.WriteLine($"오 이것도 되나{age}");
